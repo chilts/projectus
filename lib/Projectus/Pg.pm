cf273d82f8ef3f7b79f42f957ac13351ddd46d98
Andrew Chilton <chilts@appsattic.com>
2010-09-06 18:51:16 +1200

    Make the DB module a class to be inherited from

--- lib/Projectus/Pg.pm
+++ lib/Projectus/Pg.pm
@@ -2,8 +2,7 @@
 
 package Projectus::Pg;
 
-use strict;
-use warnings;
+use Moose;
 use Carp;
 use DBI;
 use Projectus::Cfg qw(get_cfg);
@@ -11,40 +10,82 @@ use Projectus::Cfg qw(get_cfg);
 use base 'Exporter';
 our @EXPORT_OK = qw(get_pg);
 
-my $pg;
+use constant NO_SLICE => { Slice => {} };
 
 ## ----------------------------------------------------------------------------
 
-sub get_pg {
-    my $cfg = get_cfg();
+has 'dbh' => (
+    is      => 'rw',
+    isa     => 'Any',
+    default => sub {
+        my $cfg = get_cfg();
 
-    return $pg if $pg;
+        warn "Should get in here at some stage";
 
-    # get any config options
-    my $db_name = $cfg->param( q{db_name} );
-    my $db_user = $cfg->param( q{db_user} );
-    my $db_pass = $cfg->param( q{db_pass} );
-    my $db_host = $cfg->param( q{db_host} );
-    my $db_port = $cfg->param( q{db_port} );
+        # get all the config options
+        my $db_name = $cfg->param( q{db_name} );
+        my $db_user = $cfg->param( q{db_user} );
+        my $db_pass = $cfg->param( q{db_pass} );
+        my $db_host = $cfg->param( q{db_host} );
+        my $db_port = $cfg->param( q{db_port} );
 
-    # make the connection string
-    my $connect_str = qq{dbi:pg:dbname=$db_name};
-    $connect_str .= qq{;host=$db_host} if $db_host;
-    $connect_str .= qq{;port=$db_port} if $db_host;
+        # make the connection string
+        my $connect_str = qq{dbi:pg:dbname=$db_name};
+        $connect_str .= qq{;host=$db_host} if $db_host;
+        $connect_str .= qq{;port=$db_port} if $db_host;
 
-    # connect to the DB
-    $pg = DBI->connect(
-        "dbi:Pg:dbname=$db_name",
-        $db_user,
-        $db_pass,
-        {
-            AutoCommit => 1, # act like psql
-            PrintError => 0, # don't print anything, we'll do it ourselves
-            RaiseError => 1, # always raise an error with something nasty
-        }
-    );
+        # connect to the DB
+        my $dbh = DBI->connect(
+            "dbi:Pg:dbname=$db_name",
+            $db_user,
+            $db_pass,
+            {
+                AutoCommit => 1, # act like psql (the spirit of least surprise)
+                PrintError => 0, # don't print anything, we'll do it ourselves
+                RaiseError => 1, # always raise an error with something nasty
+            }
+            );
 
-    return $pg;
+        return $dbh;
+    },
+);
+
+has 'tables' => (
+    is  => 'rw',
+    isa => 'HashRef',
+    default => sub { {} }, # new hashref when created
+);
+
+## ----------------------------------------------------------------------------
+
+sub register_table {
+    my ($self, $name, $prefix, @cols) = @_;
+
+    $self->tables->{$name} = {
+        name   => $name,
+        prefix => $prefix,
+        cols   => \@cols,
+    };
+}
+
+sub select_cols {
+    my ($prefix, @cols) = @_;
+    return join(', ', map { "$prefix.$_"} @cols );
+}
+
+sub row {
+    my ($self, $sql, @params) = @_;
+    return $self->dbh->selectrow_hashref( $sql, undef, @params );
+}
+
+sub rows {
+    my ($self, $sql, @params) = @_;
+    return $self->dbh->selectall_arrayref($sql, NO_SLICE, @params );
+}
+
+sub do {
+    my ($self, $sql, @params) = @_;
+    return $self->dbh->do($sql, undef, @params );
 }
 
 ## ----------------------------------------------------------------------------
