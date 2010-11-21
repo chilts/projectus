e1d4739fbfcd435f4b6b58428c1897e63394c227
Andrew Chilton <chilts@appsattic.com>
2010-11-22 12:23:35 +1300

    Add a top-level Projectus file

---
+++ lib/Projectus.pm
@@ -0,0 +1,49 @@
+## ----------------------------------------------------------------------------
+
+package Projectus;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.04';
+
+## ----------------------------------------------------------------------------
+1;
+## ----------------------------------------------------------------------------
+
+=head1 NAME
+
+Projectus - modules for use across a project, using a common configuration file
+
+=head1 VERSION
+
+Version 0.04
+
+=head1 SYNOPSIS
+
+    use Projectus;
+
+=head1 BUGS
+
+Please report bugs to C<< <chilts at appsattic dot com> >>
+
+=head1 SUPPORT
+
+None, except if you email the author.
+
+=head1 AUTHOR, COPYRIGHT & LICENSE
+
+Andrew Chilton, C<< <chilts at appsattic dot com> >>
+
+Copyright (c) 2010, Apps Attic Ltd, all rights reserved.
+
+L<http://www.appsattic.com/>
+
+This module is free software. You can redistribute it and/or modify it under
+the terms of the Artistic License 2.0.
+
+This program is distributed in the hope that it will be useful, but without any
+warranty; without even the implied warranty of merchantability or fitness for a
+particular purpose.
+
+=cut
