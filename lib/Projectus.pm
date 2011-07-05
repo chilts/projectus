511d2e342613837d9fcb067f680b8bde585e4233
Andrew Chilton <chilts@appsattic.com>
2011-07-06 08:49:11 +1200

    Prepare for new release of Projectus (v0.15.0)

--- lib/Projectus.pm
+++ lib/Projectus.pm
@@ -5,7 +5,7 @@ package Projectus;
 use strict;
 use warnings;
 
-our $VERSION = '0.14.0';
+our $VERSION = '0.15.0';
 
 ## ----------------------------------------------------------------------------
 1;
